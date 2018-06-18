f12b08636909103ecdf8f794b1a9c1fd63f60afd
Alexander Skovpen <a.n.skovpen@gmail.com>
2018-06-18 21:29:33 +0300

    fix windows tests

--- tests/testhptzip.c
+++ tests/testhptzip.c
@@ -4,10 +4,18 @@ int main(int argc, char * argv[]) {
   if(argc <= 1) {
     return 128;
   }
-  if(!strcmp(argv[1], "pack")) {
-    return PackWithZlib("test_zip/arx.zip", "test_zip/test.txt");
-  } else if(!strcmp(argv[1], "unpack")) {
-    return UnPackWithZlib("test_zip/arx.zip", NULL, "test_zip/unp");
-  }
+#ifdef WIN32
+    if(!strcmp(argv[1], "pack")) {
+      return PackWithZlib("test_zip\\arx.zip", "test_zip\\test.txt");
+    } else if(!strcmp(argv[1], "unpack")) {
+      return UnPackWithZlib("test_zip\\arx.zip", NULL, "test_zip\\unp");
+    }
+#else
+    if(!strcmp(argv[1], "pack")) {
+      return PackWithZlib("test_zip/arx.zip", "test_zip/test.txt");
+    } else if(!strcmp(argv[1], "unpack")) {
+      return UnPackWithZlib("test_zip/arx.zip", NULL, "test_zip/unp");
+    }
+#endif
   return 0;
 }
